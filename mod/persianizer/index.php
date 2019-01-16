05f4d7248434c2baf446c553679288598ff5b99c
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-01-16 05:23:31 +0330

    Fixed a bug that causes notice dumping

--- mod/persianizer/index.php
+++ mod/persianizer/index.php
@@ -4,6 +4,8 @@ use \ehsanx64\phplib\Environment;
 $e = new Environment();
 if ($e->is('wordpress')) {
     if (get_locale() === 'fa_IR') {
-      wp_enqueue_style('persianizer', EMPRESS_ASSETS . "/persianizer/persianizer.css");
+        add_action('wp_enqueue_scripts', function () {
+            wp_enqueue_style('persianizer', EMPRESS_ASSETS . "/persianizer/persianizer.css");
+        });
     }
 }
