a78b12ac918b3b1ec29ce53495e3c75f022cc200
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-02-05 12:20:21 +0330

    animate_css module now works

--- mod/animate_css/index.php
+++ mod/animate_css/index.php
@@ -1,5 +1,5 @@
 <?php
-//echo 'AnimateCSS';
-
-// Load assets
-//wp_enqueue_style('animate', EMPRESS_ASSETS . "/animate_css/animate.css");
+add_action('wp_enqueue_scripts', function () {
+	// Load assets
+	wp_enqueue_style('animate', EMPRESS_ASSETS . "/animate_css/animate.css");
+});
