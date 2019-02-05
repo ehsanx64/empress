f2a12e5216ed3b20f154123137fb7dd6eda79b43
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-02-05 19:16:57 +0330

    Fixed persianizer enqueue order bug

--- mod/persianizer/index.php
+++ mod/persianizer/index.php
@@ -6,6 +6,6 @@ if ($e->is('wordpress')) {
     if (get_locale() === 'fa_IR') {
         add_action('wp_enqueue_scripts', function () {
             wp_enqueue_style('persianizer', EMPRESS_ASSETS . "/persianizer/persianizer.css");
-        });
+        }, 1000000);
     }
 }
